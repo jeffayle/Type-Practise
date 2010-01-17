03287f3ffabfe6323daeac35e671aabcf5ec693f
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2010-01-17 00:19:58 -0500

    Startup stuff

--- typepractise.py
+++ typepractise.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 import time
+import sys
+
+#Startup stuff
+if len(sys.argv) < 2:
+    sys.stderr.write("Error: Please specify a file to practise with on the"
+            "command line\n")
+    exit()
+
+#Open the file to work with
+inputFile = open(sys.argv[1])
